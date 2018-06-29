501072f5cdb217144eaed6b089a3209abb071045
liuyang <328204086@qq.com>
2018-06-29 10:24:44 +0800

    first commit

---
+++ bill-core/src/main/java/com/bill/util/AgeUtils.java
@@ -0,0 +1,43 @@
+package com.bill.util;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * @author LiuYang
+ * @version 1.0
+ * @package: com.bill.util
+ * @description: 年龄计算工具类
+ * @date 2018-06-29 10:23
+ */
+public class AgeUtils {
+
+    public static int getAge(final Date birthDay) throws Exception {
+        Calendar cal = Calendar.getInstance();
+
+        if (cal.before(birthDay)) {
+            throw new IllegalArgumentException(
+                    "The birthDay is before Now.It's unbelievable!");
+        }
+        int yearNow = cal.get(Calendar.YEAR);
+        int monthNow = cal.get(Calendar.MONTH);
+        int dayOfMonthNow = cal.get(Calendar.DAY_OF_MONTH);
+        cal.setTime(birthDay);
+
+        int yearBirth = cal.get(Calendar.YEAR);
+        int monthBirth = cal.get(Calendar.MONTH);
+        int dayOfMonthBirth = cal.get(Calendar.DAY_OF_MONTH);
+
+        int age = yearNow - yearBirth;
+
+        if (monthNow <= monthBirth) {
+            if (monthNow == monthBirth) {
+                if (dayOfMonthNow < dayOfMonthBirth) age--;
+            }else{
+                age--;
+            }
+        }
+        return age;
+    }
+
+}
