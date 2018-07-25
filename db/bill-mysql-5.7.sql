363b9dde8d807776f076b681f0d2f83bb5442499
13227077627 <1322707627@189.cn>
2018-07-25 11:29:25 +0800

    first commit

--- db/bill-mysql-5.7.sql
+++ db/bill-mysql-5.7.sql
@@ -1,22 +1,22 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : 192.168.0.110
+Source Server         : localhost
 Source Server Version : 50717
-Source Host           : 192.168.0.110:3306
+Source Host           : localhost:3306
 Source Database       : bill
 
 Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2018-07-20 14:14:48
+Date: 2018-07-25 11:29:10
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for sys_log
+-- Table structure for `sys_log`
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_log`;
 CREATE TABLE `sys_log` (
@@ -35,7 +35,7 @@ CREATE TABLE `sys_log` (
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for sys_menu
+-- Table structure for `sys_menu`
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_menu`;
 CREATE TABLE `sys_menu` (
@@ -54,16 +54,75 @@ CREATE TABLE `sys_menu` (
 -- ----------------------------
 -- Records of sys_menu
 -- ----------------------------
-INSERT INTO `sys_menu` VALUES ('7934dc0b810011e893b8107b44518d0a', ' 添加用户页面', '920f8f147f3811e893b8107b44518d0a', 'addUser', '2', '2018-07-06 17:40:36', '2018-07-06 17:40:36', 'user:show', '2');
-INSERT INTO `sys_menu` VALUES ('7e0d8f6f841911e893b8107b44518d0a', '新增', 'ad68b690810011e893b8107b44518d0a', '', '1', '2018-07-10 16:17:31', '2018-07-10 16:17:31', 'as', '0');
+INSERT INTO `sys_menu` VALUES ('74c14bf78be911e8a9595cea1d790d8c', '新增', '92b8cee4810011e893b8107b44518d0a', '', '1', '2018-07-20 14:52:23', '2018-07-21 14:57:18', 'menu:add', '0');
 INSERT INTO `sys_menu` VALUES ('8c360f29810011e893b8107b44518d0a', '角色管理', null, null, '1', '2018-07-06 17:41:08', '2018-07-06 17:41:08', null, '1');
 INSERT INTO `sys_menu` VALUES ('920f8f147f3811e893b8107b44518d0a', '用户管理', null, null, '1', '2018-07-04 11:16:58', '2018-07-04 11:16:58', null, '1');
 INSERT INTO `sys_menu` VALUES ('92b8cee4810011e893b8107b44518d0a', '菜单管理', null, null, '1', '2018-07-06 17:41:19', '2018-07-06 17:41:19', null, '1');
-INSERT INTO `sys_menu` VALUES ('ac10fe9a841911e893b8107b44518d0a', '添加', '7934dc0b810011e893b8107b44518d0a', '', '3', '2018-07-10 16:18:48', '2018-07-10 16:18:48', '萨达', '0');
-INSERT INTO `sys_menu` VALUES ('ad68b690810011e893b8107b44518d0a', '添加角色页面', '8c360f29810011e893b8107b44518d0a', 'addRole', '2', '2018-07-06 17:42:04', '2018-07-06 17:42:04', 'role:show', '2');
+INSERT INTO `sys_menu` VALUES ('ac10fe9a841911e893b8107b44518d0a', '修改', '920f8f147f3811e893b8107b44518d0a', '', '3', '2018-07-10 16:18:48', '2018-07-21 14:56:51', 'user:update', '0');
+INSERT INTO `sys_menu` VALUES ('bb5476228bef11e881475cea1d790d8c', ' 修改', '92b8cee4810011e893b8107b44518d0a', '', '2', '2018-07-20 15:37:19', '2018-07-20 15:54:42', 'menu:add', '0');
 
 -- ----------------------------
--- Table structure for sys_user
+-- Table structure for `sys_role`
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role` (
+  `id` varchar(255) NOT NULL,
+  `role_name` varchar(255) NOT NULL,
+  `remark` varchar(255) DEFAULT NULL,
+  `create_date` datetime NOT NULL,
+  `update_date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+INSERT INTO `sys_role` VALUES ('0c42106e8fb711e893035cea1d790d8c', '发的规划', '更好', '2018-07-25 11:01:38', '2018-07-25 11:01:38');
+INSERT INTO `sys_role` VALUES ('4a0a99e58fb711e893035cea1d790d8c', '两节课', '返回', '2018-07-25 11:03:22', '2018-07-25 11:03:22');
+INSERT INTO `sys_role` VALUES ('ae80ae558fb611e893035cea1d790d8c', '阿萨德', '阿萨德', '2018-07-25 10:59:01', '2018-07-25 10:59:01');
+INSERT INTO `sys_role` VALUES ('bed7498a8fb411e893035cea1d790d8c', '规范', '多个', '2018-07-25 10:45:09', '2018-07-25 10:45:09');
+INSERT INTO `sys_role` VALUES ('d79eadab8fb611e893035cea1d790d8c', '多个', '多个', '2018-07-25 11:00:10', '2018-07-25 11:00:10');
+INSERT INTO `sys_role` VALUES ('e786f95b8fb411e893035cea1d790d8c', '规范', '多个', '2018-07-25 10:46:18', '2018-07-25 10:46:18');
+INSERT INTO `sys_role` VALUES ('f83020d38fb311e893035cea1d790d8c', 'admin', '管理员', '2018-07-25 10:39:36', '2018-07-25 10:39:36');
+INSERT INTO `sys_role` VALUES ('ff2ba05a8fb511e893035cea1d790d8c', '多个', '多个', '2018-07-25 10:54:07', '2018-07-25 10:54:07');
+
+-- ----------------------------
+-- Table structure for `sys_role_menu`
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role_menu`;
+CREATE TABLE `sys_role_menu` (
+  `role_id` varchar(255) DEFAULT NULL,
+  `menu_id` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_role_menu
+-- ----------------------------
+INSERT INTO `sys_role_menu` VALUES ('f83020d38fb311e893035cea1d790d8c', '8c360f29810011e893b8107b44518d0a');
+INSERT INTO `sys_role_menu` VALUES ('f83020d38fb311e893035cea1d790d8c', '920f8f147f3811e893b8107b44518d0a');
+INSERT INTO `sys_role_menu` VALUES ('f83020d38fb311e893035cea1d790d8c', 'ac10fe9a841911e893b8107b44518d0a');
+INSERT INTO `sys_role_menu` VALUES ('f83020d38fb311e893035cea1d790d8c', '92b8cee4810011e893b8107b44518d0a');
+INSERT INTO `sys_role_menu` VALUES ('f83020d38fb311e893035cea1d790d8c', '74c14bf78be911e8a9595cea1d790d8c');
+INSERT INTO `sys_role_menu` VALUES ('f83020d38fb311e893035cea1d790d8c', 'bb5476228bef11e881475cea1d790d8c');
+INSERT INTO `sys_role_menu` VALUES ('bed7498a8fb411e893035cea1d790d8c', '920f8f147f3811e893b8107b44518d0a');
+INSERT INTO `sys_role_menu` VALUES ('bed7498a8fb411e893035cea1d790d8c', 'ac10fe9a841911e893b8107b44518d0a');
+INSERT INTO `sys_role_menu` VALUES ('e786f95b8fb411e893035cea1d790d8c', '920f8f147f3811e893b8107b44518d0a');
+INSERT INTO `sys_role_menu` VALUES ('e786f95b8fb411e893035cea1d790d8c', 'ac10fe9a841911e893b8107b44518d0a');
+INSERT INTO `sys_role_menu` VALUES ('ff2ba05a8fb511e893035cea1d790d8c', '92b8cee4810011e893b8107b44518d0a');
+INSERT INTO `sys_role_menu` VALUES ('ff2ba05a8fb511e893035cea1d790d8c', '74c14bf78be911e8a9595cea1d790d8c');
+INSERT INTO `sys_role_menu` VALUES ('ff2ba05a8fb511e893035cea1d790d8c', 'bb5476228bef11e881475cea1d790d8c');
+INSERT INTO `sys_role_menu` VALUES ('ae80ae558fb611e893035cea1d790d8c', '92b8cee4810011e893b8107b44518d0a');
+INSERT INTO `sys_role_menu` VALUES ('ae80ae558fb611e893035cea1d790d8c', '74c14bf78be911e8a9595cea1d790d8c');
+INSERT INTO `sys_role_menu` VALUES ('ae80ae558fb611e893035cea1d790d8c', 'bb5476228bef11e881475cea1d790d8c');
+INSERT INTO `sys_role_menu` VALUES ('d79eadab8fb611e893035cea1d790d8c', '920f8f147f3811e893b8107b44518d0a');
+INSERT INTO `sys_role_menu` VALUES ('d79eadab8fb611e893035cea1d790d8c', 'ac10fe9a841911e893b8107b44518d0a');
+INSERT INTO `sys_role_menu` VALUES ('0c42106e8fb711e893035cea1d790d8c', '920f8f147f3811e893b8107b44518d0a');
+INSERT INTO `sys_role_menu` VALUES ('0c42106e8fb711e893035cea1d790d8c', 'ac10fe9a841911e893b8107b44518d0a');
+INSERT INTO `sys_role_menu` VALUES ('4a0a99e58fb711e893035cea1d790d8c', '920f8f147f3811e893b8107b44518d0a');
+INSERT INTO `sys_role_menu` VALUES ('4a0a99e58fb711e893035cea1d790d8c', 'ac10fe9a841911e893b8107b44518d0a');
+
+-- ----------------------------
+-- Table structure for `sys_user`
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_user`;
 CREATE TABLE `sys_user` (
