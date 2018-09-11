b04264d0787d2ad0c227c86c5569a2933de6ad1b
13227077627 <1322707627@189.cn>
2018-09-11 11:55:59 +0800

    first commit

---
+++ bill-core/src/main/java/com/bill/websocket/Client.java
@@ -0,0 +1,72 @@
+package com.bill.websocket;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
+public class Client {
+
+    /**
+     * Socket客户端
+     */
+  /*  public static void main(String[] args) {
+        try {
+            //创建Socket对象
+            Socket socket=new Socket("localhost",8888);
+
+            //根据输入输出流和服务端连接
+            OutputStream outputStream=socket.getOutputStream();//获取一个输出流，向服务端发送信息
+            PrintWriter printWriter=new PrintWriter(outputStream);//将输出流包装成打印流
+            printWriter.print("服务端你好，我是客户1");
+            printWriter.flush();
+            socket.shutdownOutput();//关闭输出流
+
+            InputStream inputStream=socket.getInputStream();//获取一个输入流，接收服务端的信息
+            InputStreamReader inputStreamReader=new InputStreamReader(inputStream);//包装成字符流，提高效率
+            BufferedReader bufferedReader=new BufferedReader(inputStreamReader);//缓冲区
+            String info="";
+            String temp=null;//临时变量
+            while((temp=bufferedReader.readLine())!=null){
+                info+=temp;
+                System.out.println("客户端接收服务端发送信息："+info);
+            }
+
+            //关闭相对应的资源
+            bufferedReader.close();
+            inputStream.close();
+            printWriter.close();
+            outputStream.close();
+            socket.close();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }*/
+    public static void main(String[] args) {
+        try {
+            Socket s = new Socket("127.0.0.1",8888);
+
+            //构建IO
+            InputStream is = s.getInputStream();
+            OutputStream os = s.getOutputStream();
+
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os));
+            //向服务器端发送一条消息
+            bw.write("测试客户端和服务器通信，服务器接收到消息返回到客户端\n");
+            bw.flush();
+
+            //读取服务器返回的消息
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String mess = br.readLine();
+            System.out.println("服务器："+mess);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ bill-core/src/main/java/com/bill/websocket/Server.java
@@ -0,0 +1,46 @@
+package com.bill.websocket;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    /**
+     * Socket服务端
+     */
+ /*   public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(8888);
+            System.out.println("服务端已启动，等待客户端连接..");
+
+            while (true) {
+                Socket socket = serverSocket.accept();// 侦听并接受到此套接字的连接,返回一个Socket对象
+                SocketThread socketThread = new SocketThread(socket);
+                socketThread.start();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }*/
+    public static void main(String[] args) {
+        try {
+            ServerSocket ss = new ServerSocket(8888);
+            System.out.println("启动服务器....");
+            Socket s = ss.accept();
+            System.out.println("客户端:"+s.getInetAddress().getLocalHost()+"已连接到服务器");
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            //读取客户端发送来的消息
+            String mess = br.readLine();
+            System.out.println("客户端："+mess);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+            bw.write(mess+"\n");
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
