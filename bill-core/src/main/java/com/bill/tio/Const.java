5d87e9b712c3dd20b87a10ea3c4aa08dbda9cf4d
13227077627 <1322707627@189.cn>
2018-09-11 12:02:38 +0800

    first commit

---
+++ bill-core/src/main/java/com/bill/tio/Const.java
@@ -0,0 +1,18 @@
+package com.bill.tio;
+
+public class Const {
+    /**
+     * 服务器地址
+     */
+    public static final String SERVER = "127.0.0.1";
+
+    /**
+     * 监听端口
+     */
+    public static final int PORT = 6789;
+
+    /**
+     * 心跳超时时间
+     */
+    public static final int TIMEOUT = 5000;
+}
